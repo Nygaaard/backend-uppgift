1e372aac62615bbced33c1302d0ed6e4c1e5381d
Andreas Nygård <anny2301@student.miun.se>
2024-03-31 20:21:14 +0200

    Added functionality for adding courses

--- server.js
+++ server.js
@@ -26,6 +26,46 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
+//Skapa nytt inlägg
+app.post("/addcourses", (req, res) => {
+  //Variabler för input-värden
+  let coursecode = req.body.coursecode;
+  let coursename = req.body.coursename;
+  let progression = req.body.progression;
+  let syllabus = req.body.syllabus;
+  let err = "";
+
+  //Kontrollera att alla input-fält är ifyllda
+  if (
+    coursecode != "" &&
+    coursename != "" &&
+    progression != "" &&
+    syllabus != ""
+  ) {
+    //Lagra i databasen om korrekt
+    //Skriver inte in värden direkt i SQL-fråga
+    const stmt = db.prepare(
+      `INSERT INTO courses (
+        coursecode,
+        coursename,
+        progression,
+        syllabus
+      )
+      VALUES (?, ?, ?, ?);
+      `
+    );
+    //Värden
+    stmt.run(coursecode, coursename, progression, syllabus);
+    stmt.finalize();
+  } else {
+    err = "Du måste fylla i alla textfält";
+  }
+
+  res.render("addcourses", {
+    err: err,
+  });
+});
+
 //Starta programmet
 app.listen(port, () => {
   console.log(`Application started on port ${port}`);
